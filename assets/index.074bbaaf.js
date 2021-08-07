4a126ecffe794df40788c54ecca8999563b0c3b5
Jason <jason.obeid98@gmail.com>
2021-08-07 12:15:14 -0400

    Updates

---
+++ assets/index.074bbaaf.js
@@ -0,0 +1,1 @@
+import{R as e,a as t}from"./vendor.867f8aa9.js";function n(){return e.createElement("p",null,"test")}t.render(e.createElement(e.StrictMode,null,e.createElement(n,null)),document.getElementById("root"));
